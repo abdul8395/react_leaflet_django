3dcd2b58fa207044bb40741f57af5d0d2a68ee2b
abdul8395 <61780062+abdul8395@users.noreply.github.com>
2022-03-26 01:02:57 +0500

    on map click modal opening and on save function calling

--- src/Map_component.js
+++ src/Map_component.js
@@ -28,18 +28,13 @@ export default class Map extends Component {
   closeModal = () => this.setState({ isOpen: false });
   
   componentDidMount (){
-  
-
-
     var map = L.map('map').setView([53.16292816226638, -7.8925780579447755], 7);
 
-   
     var googlestreet   = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
         }).addTo(map)
     var dark  = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png');
-    // https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
     var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
                 maxZoom: 20,
                 subdomains:['mt0','mt1','mt2','mt3']
@@ -56,15 +51,9 @@ export default class Map extends Component {
     setTimeout(
       function() {
         map.on('click', function(e){
-          // this.setState({ isOpen: true })
-            // test()
             $('#mymodalopenbtn').trigger( "click" );
-            
-
             // L.marker(e.latlng).addTo(map);
-            // rowcenter.push([e.latlng.lat,e.latlng.lng])
             console.log(e.latlng.lat + ", " + e.latlng.lng)
-            // alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
         });
       }
       .bind(this),
@@ -90,7 +79,6 @@ export default class Map extends Component {
             <Button variant="success" onClick={()=>{savefunc()}}>Save</Button>
           </Modal.Footer>
         </Modal>
-       
         </>
     )
     // return <Wrapper height="100vh" id="map" />;
